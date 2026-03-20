7fd1aa922ae87c08adac9dc43eafc0ac5957bd1e
ahnafahmed-git <ahmedahnaf513@gmail.com>
2026-03-21 00:33:11 +0600

    Delete Install Dependencies.py

--- Install Dependencies.py
+++
@@ -1,9 +0,0 @@
-!pip install numpy==1.23.5 -q   # NVIDIA repo needs older numpy
-!git clone https://github.com/NVIDIA/tacotron2.git
-%cd tacotron2
-!git submodule init
-!git submodule update
-!pip install inflect==5.6.0 librosa Unidecode -q
-!pip install pesq pystoi scipy -q
-!git clone https://github.com/aliutkus/speechmetrics.git
-%cd speechmetrics && pip install -e . -q && %cd ..
